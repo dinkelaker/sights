48f06134b2a202671089183f3686eb71fbff8c12
Tom Dinkelaker <tom.dinkelaker@gmail.com>
2020-05-24 18:36:54 +0200

    Save sight in SQLite database
    - Create a table if not existing
    - Add row in table and replace old data

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
+import 'package:sights/helpers/db_helper.dart';
 
 import '../models/place.dart';
 
@@ -9,11 +10,27 @@ class GreatPlaces extends ChangeNotifier {
 
   List<Place> get items {
     return _items;
-  } 
+  }
 
-  void addSight(String title, File image,) {
-    final newSight = Place(id: DateTime.now().toIso8601String(), image: image, location: null, title: title, );
+  void addSight(
+    String title,
+    File image,
+  ) {
+    final newSight = Place(
+      id: DateTime.now().toIso8601String(),
+      image: image,
+      location: null,
+      title: title,
+    );
     _items.add(newSight);
     notifyListeners();
+    DBHelper.insert(
+      'sights',
+      {
+        'id': newSight.id,
+        'title': newSight.title,
+        'image': newSight.image.path,
+      },
+    );
   }
 }
